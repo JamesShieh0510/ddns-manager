fefed12ed7a46353be1e3778d9405e4983f005c5
James Shieh <jamesshieh0510@gmail.com>
2021-01-12 00:15:33 +0800

    update

--- clientCronJob.py
+++ clientCronJob.py
@@ -4,9 +4,9 @@ import networkManager
 import networkManagerFirebasePlugin
 import threading
 ip=''
-
+ip_record_file='./previous-ip'
 def UpdateIP(ip):
-    current_ip=networkManager.getMyIP()
+    current_ip = networkManager.getMyIP()
     needUpdate = ~(ip == current_ip)
     if ~needUpdate:
         print('your ip hasn\'t changed:', ip)
@@ -17,8 +17,14 @@ def UpdateIP(ip):
         mailManager.sendEmail(email, email, "your IP has updated.", ip)
         networkManagerFirebasePlugin.updateIP()
         print("your IP has updated.", ip)
+        fp = open(ip_record_file,'w')
+        fp.write(ip)
+        fp.close()
     return needUpdate
 
 networkManagerFirebasePlugin.initApp()
 print("Current Time: ", init.logManager.getTimestamp())   
-UpdateIP(ip)
+with open(ip_record_file) as ipRecord:
+    ip = ipRecord.read()
+    ipRecord.close()
+    UpdateIP(ip)
