93b6a7db885ce46b356a4e956e38d46c67ffae62
James Shieh <jamesshieh0510@gmail.com>
2021-01-11 22:50:08 +0800

    update

--- setup-ddns-server.sh
+++ setup-ddns-server.sh
@@ -12,5 +12,5 @@ export nginx_config_path=$nginx_config
 sudo chown root:root $nginx_config
 sudo chmod 707 $nginx_config
 su root
-sudo echo "$python_user  ALL=(ALL) NOPASSWD: /restart-nginx-service.sh" >> /etc/sudoers
+sudo echo "$python_user  ALL=(ALL) NOPASSWD: /home/$python_user/restart-nginx-service.sh" >> /etc/sudoers
 sudo echo "$python_user  ALL=(ALL) NOPASSWD: $nginx_config_path" >> /etc/sudoers
